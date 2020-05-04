622c9829b5070b053877fca7d036fd0324117231
Evgeny Moskovtsev <apelvery@gmail.com>
2020-05-04 12:11:28 +0300

    Added length and girth buttons

--- app.py
+++ app.py
@@ -18,9 +18,14 @@ class App:
         # self.canvasPanel.pack(side="right", fill="both", expand=True)
         self.buttonPanel.grid(row=0, column=1)
         self.canvasPanel.grid(row=0, column=0)
-
-        self.btn_open = tkinter.Button(self.buttonPanel, text="Select_image", width=50, command=lambda: self.select_images())
+        self.btn_open = tkinter.Button(self.buttonPanel, text="Select_image", width=50,
+                                       command=lambda: self.select_images())
         self.btn_open.grid(row=0, column=0)
+        self.x1 = None
+        self.y1 = None
+        self.x2 = None
+        self.y2 = None
+        self.info_label_flag = True
 
     def select_images(self):
         self.btn_open.grid_remove()
@@ -29,7 +34,7 @@ class App:
 
         self.canvas = tkinter.Canvas(self.canvasPanel, width=self.image.shape[1], height=self.image.shape[0])
         self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.image))
-        self.canvas.create_image(0, 0,  image=self.photo, anchor=tkinter.NW)
+        self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
         # self.canvas.pack(fill=tkinter.BOTH, expand=1)
         # self.btn_open.forget()
         self.canvas.grid(row=0, column=0)
@@ -50,18 +55,19 @@ class App:
         self.substr_photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(result))
         self.canvas.create_image(0, 0, image=self.substr_photo, anchor=tkinter.NW)
         # self.canvas.pack()
+
     def func_dim(self):
         self.btn_set_reference_size = tkinter.Button(self.buttonPanel, width=20,
                                                      text="Задать эталонный размер",
-                                                     background='green',activebackground='green',
+                                                     background='green', activebackground='green',
                                                      command=lambda: self.set_size())
 
-        self.info_label = tkinter.Label(self.buttonPanel,text='Отметьте 2 точки на \nизображении,\n'
-                                             'расстояние между\n которыми будет\n '
-                                             'принято,''как эталонное', font=("Arial 32", 8))
-
         self.btn_set_reference_size.grid(row=2, column=1)
-        self.info_label.grid(row=3, column=1)
+        if self.info_label_flag is True:
+            self.info_label = tkinter.Label(self.buttonPanel, text='Отметьте 2 точки на \nизображении,\n'
+                                                                   'расстояние между\n которыми будет\n '
+                                                                   'принято,''как эталонное', font=("Arial 32", 8))
+            self.info_label.grid(row=3, column=1)
         # self.btn_set_reference_size.pack()
         # self.info_label.pack()
 
@@ -71,32 +77,71 @@ class App:
     def draw_line(self, event):
 
         if self.first_click is True:
-            self.x1 = event.x
-            self.y1 = event.y
-            self.first_click = False
+            if self.x1 is None and self.y1 is None:
+                self.x1 = event.x
+                self.y1 = event.y
+                self.first_click = False
 
         else:
-            self.x2 = event.x
-            self.y2 = event.y
-            self.canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill='yellow', width=2)
-            self.info_label.destroy()
-            self.btn_cancel_draw = tkinter.Button(self.buttonPanel, width=20,
-                                                         text="Отмена",
-                                                         background='red', activebackground='red',
-                                                         command=lambda: self.cancel())
-            self.btn_cancel_draw.grid(row=3, column=1)
+            if self.x2 is None and self.y2 is None:
+                self.x2 = event.x
+                self.y2 = event.y
+                self.canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill='yellow', width=2)
+                self.info_label.destroy()
+                self.btn_cancel_draw = tkinter.Button(self.buttonPanel, width=20,
+                                                      text="Отмена",
+                                                      background='red', activebackground='red',
+                                                      command=lambda: self.cancel())
+                self.btn_cancel_draw.grid(row=2, column=2)
+
+                # Длина, обхват
+                self.length_btn = tkinter.Button(self.buttonPanel, width=20,
+                                                 text="Длина",
+                                                 background='green', activebackground='green',
+                                                 command=lambda: self.set_length())
+                self.girth_btn = tkinter.Button(self.buttonPanel, width=20,
+                                                text="Обхват",
+                                                background='green', activebackground='green',
+                                                command=lambda: self.set_girth())
+                self.length_btn.grid(row=3, column=1)
+                self.girth_btn.grid(row=3, column=2)
 
     def cancel(self):
+        self.first_click = True
+        self.info_label_flag = False
+        self.x1, self.y1, self.x2, self.y2 = None, None, None, None
         self.substr()
+        self.func_dim()
+        # self.btn_func_dim.flash()
         self.btn_cancel_draw.grid_remove()
 
+    def set_length(self):
+        self.girth_btn.grid_remove()
+        self.length_entry = tkinter.Entry(self.buttonPanel, width=20)
+        self.length_entry.grid(row=4, column=1)
+        self.ok_btn = tkinter.Button(self.buttonPanel, width=10,
+                                     text="Ок",
+                                     background='green', activebackground='green',
+                                     command=lambda: self.set_text_lenght())
+        self.ok_btn.grid(row=5, column=1)
 
+    def set_girth(self):
+        self.length_btn.grid_remove()
+        self.girth_entry = tkinter.Entry(self.buttonPanel, width=20)
+        self.girth_entry.grid(row=4, column=2)
+        self.ok_btn = tkinter.Button(self.buttonPanel, width=10,
+                                     text="Ок",
+                                     background='green', activebackground='green',
+                                     command=lambda: self.set_text_lenght())
 
+    def set_text_lenght(self):
+        self.text = self.length_entry.get()
+        print(self.text)
 
+    def set_text_girth(self):
+        self.text= self.girth_entry.get()
+        print(self.text)
 
-
-# Create a window and pass it to the Application object
 root = tkinter.Tk()
-
 gui = App(root, '')
 root.mainloop()
